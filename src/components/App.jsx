5e7e70c18ab96eece448192be930fbb748f06a28
Tobias Alves <tobiasjose12@gmail.com>
2024-05-06 07:22:14 -0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 
 function App () {
   const [inputName, setInputName] = useState ('');
-
   const [displayName, setDisplayName] = useState ('');
 
   function handleChange (event) {
